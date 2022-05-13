0d6fd0140f4aed483bfa2f57829e583604572571
Le Duc Dung <dungldc6ts42020@gmail.com>
2022-05-13 10:29:20 +0700

    all in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular_we16304';
+
+  teachers= [
+    {
+      id: 1,
+      name: 'Teacher A',
+      age: 23,
+      gender: 1,
+      avatar: 'https://placeimg.com/640/480/any',
+      status: 1
+    },
+    {
+      id: 2,
+      name: 'Teacher E',
+      age: 25,
+      gender: 1,
+      avatar: 'https://placeimg.com/640/480/any',
+      status: 1
+    },
+    {
+      id: 3,
+      name: 'Teacher B',
+      age: 30,
+      gender: 0,
+      avatar: 'https://placeimg.com/640/480/any',
+      status: 0
+    },
+    {
+      id: 4,
+      name: 'Teacher C',
+      age: 27,
+      gender: 1,
+      avatar: 'https://placeimg.com/640/480/any',
+      status: 1
+    },
+    {
+      id: 5,
+      name: 'Teacher D',
+      age: 29,
+      gender: 0,
+      avatar: 'https://placeimg.com/640/480/any',
+      status: 0
+    },
+  ]
+  StudentName = "DUNGLD2708"
+  StudentId = "PH17915"
 }
