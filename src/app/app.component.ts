3e9fc17c67bd7864d22972ef4cb9c9451604de53
Le Duc Dung <dungldc6ts42020@gmail.com>
2022-05-18 09:38:52 +0700

    feat: event+form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -52,4 +52,56 @@ export class AppComponent {
   ]
   StudentName = "DUNGLD2708"
   StudentId = "PH17915"
+
+  studentName = ''
+  ClickH1 = ()=> {
+    console.log('Đã Click vào');
+    this.studentName= 'Poly'
+  }
+
+  ShowStatus = true
+  changeTableStatus= ()=> {
+    this.ShowStatus = !this.ShowStatus
+  }
+
+// Định nghĩa hàm nhận giá trị từ các input
+
+  inputValues= {
+    name: '',
+    age: '',
+    avatar: '',
+    gender: '0',
+    status: '0'
+  }  
+  // onInputName= (event:any,infor:string) =>{
+  //   this.inputValues.name= event.target.value;
+  // }
+  // onInputAge= (event:any, infor:string) =>{
+  //   this.inputValues.age= event.target.value;
+  // }
+
+  //key: 'name'|'age' => key chỉ được là giá trị 'name' hoặc 'age'
+  onInput= (event:any, key: 'name' | 'age' | 'avatar' | 'gender' | 'status') => {
+    this.inputValues[key]= event.target.value
+  }
+  onSubmit=() =>{
+    console.log(this.inputValues)
+    // Thêm dữ liệu vừa thao tác trên form vào mảng teachers
+    this.teachers.push({
+      ...this.inputValues,
+      age: +this.inputValues.age,
+      gender: +this.inputValues.gender,
+      status: +this.inputValues.status,
+      id: this.teachers.length +1,
+    });
+    // Cập nhật lại giá trị cho input ở form:
+    this.inputValues = {
+      name : '',
+      age: '',
+      avatar: '',
+      gender: '0',
+      status: '0'
+    }
+  }
+  
 }
