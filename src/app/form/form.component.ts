41450e0a57308599c156f253d1b42cdb669b94e6
Le Duc Dung <dungldc6ts42020@gmail.com>
2022-05-22 18:08:08 +0700

    add, del form angular

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,19 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+    selector: 'app-form',
+    templateUrl: './form.component.html',
+    styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
-  onSubmit(formData:{name:string, age:number}){
-    console.log(formData);
-    
-  }
+    ngOnInit(): void {
+    }
+
+    inputValues = {
+        id: 0,
+        name: '',
+        age: 0
+    }
+
+    users = [
+        {
+            id: 1,
+            name: 'dungldph17915',
+            age: 20
+        },
+        {
+            id: 2,
+            name: 'dungldph17920',
+            age: 21
+        }
+    ]
+
+    onSubmit(userForm: NgForm) {
+        // console.log(formData);
+        // Tìm ra id lớn nhất
+        const userIds = this.users
+            .map(user => user.id)
+            .sort((a, b) => a - b)
+        // console.log(userIds)
+        const newId = userIds[userIds.length - 1]
+        this.users.push({
+            ...userForm.value,
+            id: newId + 1
+        });
+        userForm.resetForm({
+            name: '',
+            age: 0,
+        })
+    }
+    onDelete(userId: number) {
+        this.users = this.users.filter((user)=>user.id !== userId)
+    }
+    onEdit(userId: number){
+        const editUser = this.users.find((user) => user.id === userId)
+        console.log(editUser);
+        if(editUser){
+            this.inputValues = {...editUser}
+        }
+        
+    }
 
 }
